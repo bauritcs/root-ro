07bca05cf6d93b0860a9233c2fe04d68eeee1f2a
Stefan Baur <sendspamhere@stefanbaur.de>
2020-01-31 17:17:10 +0000

    changed script to work with Debian Buster and grub2

--- install.sh
+++ install.sh
@@ -9,33 +9,17 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
-echo Creating initramfs...
-mkinitramfs -o /boot/init.gz
-
-if ! grep -q "^initramfs " /boot/config.txt; then
-  echo Adding \"initramfs init.gz\" to /boot/config.txt
-  echo initramfs init.gz >> /boot/config.txt
-fi
-
 if ! grep -q "^overlay" /etc/initramfs-tools/modules; then
   echo Adding \"overlay\" to /etc/initramfs-tools/modules
   echo overlay >> /etc/initramfs-tools/modules
 fi
 
-
-
-echo Disabling swap, we dont want swap files in a read-only root filesystem...
-dphys-swapfile swapoff
-dphys-swapfile uninstall
-update-rc.d dphys-swapfile disable
-systemctl disable dphys-swapfile
-
 echo Setting up maintenance scripts in /root...
-cp reboot-to-readonly-mode.sh /root/reboot-to-readonly-mode.sh
-chmod +x /root/reboot-to-readonly-mode.sh
+cp reboot-to-readonly-mode.sh /usr/local/sbin/reboot-to-readonly-mode.sh
+chmod +x /usr/local/sbin/reboot-to-readonly-mode.sh
 
-cp reboot-to-writable-mode.sh /root/reboot-to-writable-mode.sh
-chmod +x /root/reboot-to-writable-mode.sh
+cp reboot-to-writable-mode.sh /usr/local/sbin/reboot-to-writable-mode.sh
+chmod +x /usr/local/sbin/reboot-to-writable-mode.sh
 
 echo Setting up initramfs-tools scripts...
 cp etc/initramfs-tools/scripts/init-bottom/root-ro /etc/initramfs-tools/scripts/init-bottom/root-ro
@@ -46,18 +30,11 @@ chmod +x /etc/initramfs-tools/hooks/root-ro
 
 echo Updating initramfs...
 update-initramfs -u
-mkinitramfs -o /boot/init.gz
 
-if ! grep -q "root-ro-driver=overlay" /boot/cmdline.txt; then
-  echo Adding root-ro-driver parameter to /boot/cmdline.txt
-  sed -i "1 s|$| root-ro-driver=overlay|" /boot/cmdline.txt
+if ! grep GRUB_CMDLINE_LINUX /etc/default/grub | grep -q "root-ro-driver=overlay" ; then
+  echo Adding root-ro-driver parameter to /etc/default/grub
+  echo 'GRUB_CMDLINE_LINUX_DEFAULT+=" root-ro-driver=overlay "' >>/etc/default/grub
 fi
 
-echo Removing the random seed file
-systemctl stop systemd-random-seed.service
-if [ -f /var/lib/systemd/random-seed ]; then
-  rm -f /var/lib/systemd/random-seed
-fi
-
-echo Restarting RPI
+echo Restarting ...
 reboot
