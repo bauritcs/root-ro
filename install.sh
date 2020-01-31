2675cbf245bdf997a3c484962c9de82dac2cf186
Stefan Baur <sendspamhere@stefanbaur.de>
2020-01-31 17:23:20 +0000

    Messages fixed.

--- install.sh
+++ install.sh
@@ -10,29 +10,29 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 if ! grep -q "^overlay" /etc/initramfs-tools/modules; then
-  echo Adding \"overlay\" to /etc/initramfs-tools/modules
+  echo Adding \"overlay\" to /etc/initramfs-tools/modules ...
   echo overlay >> /etc/initramfs-tools/modules
 fi
 
-echo Setting up maintenance scripts in /root...
+echo Setting up maintenance scripts in /usr/local/sbin ...
 cp reboot-to-readonly-mode.sh /usr/local/sbin/reboot-to-readonly-mode.sh
 chmod +x /usr/local/sbin/reboot-to-readonly-mode.sh
 
 cp reboot-to-writable-mode.sh /usr/local/sbin/reboot-to-writable-mode.sh
 chmod +x /usr/local/sbin/reboot-to-writable-mode.sh
 
-echo Setting up initramfs-tools scripts...
+echo Setting up initramfs-tools scripts ...
 cp etc/initramfs-tools/scripts/init-bottom/root-ro /etc/initramfs-tools/scripts/init-bottom/root-ro
 chmod +x /etc/initramfs-tools/scripts/init-bottom/root-ro
 
 cp etc/initramfs-tools/hooks/root-ro /etc/initramfs-tools/hooks/root-ro
 chmod +x /etc/initramfs-tools/hooks/root-ro
 
-echo Updating initramfs...
+echo Updating initramfs ...
 update-initramfs -u
 
 if ! grep GRUB_CMDLINE_LINUX /etc/default/grub | grep -q "root-ro-driver=overlay" ; then
-  echo Adding root-ro-driver parameter to /etc/default/grub
+  echo Adding root-ro-driver parameter to /etc/default/grub ...
   echo 'GRUB_CMDLINE_LINUX_DEFAULT+=" root-ro-driver=overlay "' >>/etc/default/grub
 fi
 
